3e451a8ff4ca33ae08c00ee6fa9cd0109bd4fa5d
Lin2Jing4 <35728132+Lin2Jing4@users.noreply.github.com>
2020-11-13 20:19:03 +0800

    rename function "percentialize" to "percentalize"
    Google search shows 69 results for "percentalize" and none at all for "percentialize."

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 var originEncodeUri = encodeURI;
 var originEncodeUriComponent = encodeURIComponent;
 
-function percentialize(x) {
+function percentalize(x) {
 	return '%' + x.charCodeAt(0).toString(16).toUpperCase();
 }
 
 encodeURI = (function(){
 	return function encodeURI(s) {
-		return originEncodeUri.apply(this, arguments).replace(/[]/g, percentialize);
+		return originEncodeUri.apply(this, arguments).replace(/[]/g, percentalize);
 	}
 })();
 
 encodeURIComponent = (function(){
 	return function encodeURIComponent(s) {
-		return originEncodeUriComponent.apply(this, arguments).replace(/[!'()*]/g, percentialize);
+		return originEncodeUriComponent.apply(this, arguments).replace(/[!'()*]/g, percentalize);
 	}
 })();
 
